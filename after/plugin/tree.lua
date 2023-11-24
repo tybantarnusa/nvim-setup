c18a09da4bd03b3fc47f96a810c82a38953aeafa
Thoyib Antarnusa <thoyib@kata.ai>
2023-11-25 02:31:55 +0700

    setup nvim

---
+++ after/plugin/tree.lua
@@ -0,0 +1,29 @@
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- set termguicolors to enable highlight groups
+vim.opt.termguicolors = true
+
+-- empty setup using defaults
+require("nvim-tree").setup()
+
+-- OR setup with some options
+require("nvim-tree").setup({
+  sort = {
+    sorter = "case_sensitive",
+  },
+  view = {
+    width = 30,
+  },
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = true,
+  },
+})
+
+vim.keymap.set('n', '<leader><leader>', ':NvimTreeToggle<cr>', defaults)
+vim.keymap.set('n', '<C-,>', '<C-w><Left>', defaults)
+vim.keymap.set('n', '<C-.>', '<C-w><Right>', defaults)
---
+++ init.lua
@@ -0,0 +1,16 @@
+require('plugins')
+
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")

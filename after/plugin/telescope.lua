de8bbeef4e8b02b86db6b284d8e3d031313b6aa2
Thoyib Antarnusa <thoyib@kata.ai>
2024-03-25 13:59:37 +0700

    Add telescope file browser

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -9,12 +9,21 @@ telescope.setup {
                 "node_modules", ".git"
             }
         }
-    }
+    },
+    extensions = {
+        file_browser = {
+            theme = "ivy",
+            hijack_netrw = true,
+        },
+    },
 }
 
+telescope.load_extension "file_browser"
+
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fb', ':Telescope file_browser<CR><ESC>', {})
 vim.keymap.set('n', '<leader>fa', ':Telescope git_files<CR>', {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fs', function()
-	builtin.grep_string({ search = vim.fn.input("Search in files: ") })
+    builtin.grep_string({ search = vim.fn.input("Search in files: ") })
 end)
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -25,10 +25,10 @@ return require('packer').startup(function(use)
         "williamboman/mason-lspconfig.nvim",
         "neovim/nvim-lspconfig",
     }
-    use 'hrsh7th/nvim-cmp'       -- Autocompletion plugin
-    use 'hrsh7th/cmp-nvim-lsp'   -- LSP source for nvim-cmp
+    use 'hrsh7th/nvim-cmp'         -- Autocompletion plugin
+    use 'hrsh7th/cmp-nvim-lsp'     -- LSP source for nvim-cmp
     use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
-    use 'L3MON4D3/LuaSnip'       -- Snippets plugin
+    use 'L3MON4D3/LuaSnip'         -- Snippets plugin
     use {
         'nvim-tree/nvim-tree.lua',
         requires = {
@@ -49,4 +49,8 @@ return require('packer').startup(function(use)
     })
     use 'Exafunction/codeium.vim'
     use 'f-person/git-blame.nvim'
+    use {
+        "nvim-telescope/telescope-file-browser.nvim",
+        requires = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" }
+    }
 end)
